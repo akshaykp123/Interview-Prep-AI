8782130b083bee2f75d861c248c694432cc26561
akshaykp123 <akshaykp701@gmail.com>
2025-09-28 13:06:32 +0530

    changed

--- frontend/src/components/Loader/SpinnerLoader.jsx
+++ frontend/src/components/Loader/SpinnerLoader.jsx
@@ -5,7 +5,7 @@ const SpinnerLoader = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        class="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-cyan-900"
+        className="inline w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-cyan-900"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@ const SpinnerLoader = () => {
           fill="currentFill"
         />
       </svg>
-      <span class="sr-only">Loading...</span>
+      <span className="sr-only">Loading...</span>
     </div>
   )
 }
